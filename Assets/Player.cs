1a7ba50721da80b8a7e77a115528084aad29040d
park8306 <kosaf0804@gmail.com>
2021-07-09 19:55:59 +0900

    초기 커밋

---
+++ Assets/Player.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 20;
+    public float moveableDistance = 3;
+    public Transform mousePointer;
+
+    Plane plane = new Plane(new Vector3(0, 1, 0), 0);
+    void Update()
+    {
+        //RaycastHit hit;
+        Move();
+        Jump();
+    }
+    public AnimationCurve jumpYac;
+    private void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            StartCoroutine(JumpCo());
+        }
+    }
+    public float jumpYMultiply = 1;
+    private IEnumerator JumpCo()
+    {
+        float jumpStartTime = Time.time;
+        float jumpDuration = jumpYac[jumpYac.length - 1].time;
+        float jumpEndTime = jumpStartTime + jumpDuration;
+        float sumEvaluateTime = 0;
+        while (Time.time < jumpEndTime)
+        {
+            float y = jumpYac.Evaluate(sumEvaluateTime);
+            y *= jumpYMultiply;
+            transform.Translate(0, y, 0);
+            yield return null;
+            sumEvaluateTime += Time.deltaTime;
+        }
+    }
+
+    private void Move()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out float enter))
+        {
+            Vector3 hitPoint = ray.GetPoint(enter);
+            mousePointer.position = hitPoint;
+            float distance = Vector3.Distance(hitPoint, transform.position);
+            if (distance > moveableDistance)
+            {
+                var dir = hitPoint - transform.position;
+                dir.Normalize();
+                transform.Translate(dir * speed * Time.deltaTime);
+            }
+        }
+    }
+}
