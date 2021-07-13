192becb0d76ae24e02a7c7831386b3239b095863
park8306 <kosaf0804@gmail.com>
2021-07-14 00:46:58 +0900

    2021-07-13

---
+++ Assets/Goblin.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goblin : MonoBehaviour
+{
+    // 추격 할대 플레이어한테 공격 가능한 거리면 공격.
+    // 공격후 추격
+    // 추격 공격
+
+    Animator animator;
+    IEnumerator Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        player = Player.instance;
+
+        CurrentFsm = IdleFSM;
+        while (true) // 상태를 무한히 반복해서 실행하는 부분.
+        {
+            fsmHandle = StartCoroutine(CurrentFsm());
+            while (fsmHandle != null)
+            {
+                yield return null;
+            }
+        }
+    }
+    Coroutine fsmHandle;
+    Func<IEnumerator> CurrentFsm
+    {
+        get { return m_currentFsm; }
+        set
+        {
+            m_currentFsm = value;
+            fsmHandle = null;
+        }
+    }
+    Func<IEnumerator> m_currentFsm;
+    Player player;
+    public float detectRange = 40;
+    public float attackRange = 10;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, detectRange);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+    private IEnumerator IdleFSM()
+    {
+        // 시작하면 Idle <- Idle 애니메이션 재생.
+        animator.Play("Idle");
+
+        ////IdleCo
+        // 플레이어 근접하면 추격
+        while (Vector3.Distance(transform.position, player.transform.position)
+            > detectRange)
+        {
+            yield return null;
+        }
+        CurrentFsm = ChaseFSM;
+    }
+    public float speed = 34;
+    private IEnumerator ChaseFSM()
+    {
+        animator.Play("Run");
+        while (true)
+        {
+            Vector3 toPlayerDirection = player.transform.position - transform.position;
+            toPlayerDirection.Normalize();
+            transform.Translate(toPlayerDirection * speed * Time.deltaTime, Space.World);
+            bool isRightSide = toPlayerDirection.x > 0;
+            if (isRightSide)
+            {
+                transform.rotation = Quaternion.Euler(Vector3.zero);
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+
+            if (Vector3.Distance(transform.position, player.transform.position) < attackRange)
+            {
+                CurrentFsm = AttackFSM;
+                yield break;
+            }
+            yield return null;
+        }
+    }
+    public float attackTime = 0.7f;
+    public float attackApplyTime = 0.2f;
+    public int power = 10;
+    private IEnumerator AttackFSM()
+    {
+        animator.Play("Attack");
+        yield return new WaitForSeconds(attackApplyTime);
+        // 실제 어택
+        if (Vector3.Distance(player.transform.position, transform.position) < attackRange)
+        {
+            // 플레이어를 때리자.
+            player.TakeHit(power);
+             //TakeHit <- 공격 당할 때
+        }
+        yield return new WaitForSeconds(attackTime - attackApplyTime);
+        CurrentFsm = ChaseFSM;
+    }
+    public float hp = 100;
+    internal void TakeHit(float damage)
+    {
+        hp -= damage;
+        StopCoroutine(fsmHandle);
+        CurrentFsm = TakeHitFSM;
+    }
+    public float takeHitTime = 0.3f;
+    private IEnumerator TakeHitFSM()
+    {
+        animator.Play("TakeHit");
+        yield return new WaitForSeconds(takeHitTime);
+        if (hp > 0)
+            CurrentFsm = IdleFSM;
+        else
+            CurrentFsm = DeathFSM;
+    }
+
+    public float deathTime = 0.5f;
+    private IEnumerator DeathFSM()
+    {
+        animator.Play("Death");
+        yield return new WaitForSeconds(deathTime);
+        Destroy(gameObject);
+    }
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,9 +3,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
+    public static Player instance;
+    private void Awake()
+    {
+        instance = this;
+    }
     [SerializeField] StateType state = StateType.Idle;
     public float speed = 20;
     public float walkDistance = 12;
@@ -15,7 +21,7 @@ public class Player : MonoBehaviour
     public Transform spriteTr;
     SpriteTrailRenderer.SpriteTrailRenderer spriteTrailRenderer;
     Plane plane = new Plane(new Vector3(0, 1, 0), 0);
-
+    NavMeshAgent agent;
     private void Start()
     {
         animator = GetComponentInChildren<Animator>();
@@ -23,14 +29,63 @@ public class Player : MonoBehaviour
         normalSpeed = speed;
         spriteTrailRenderer = GetComponentInChildren<SpriteTrailRenderer.SpriteTrailRenderer>();
         spriteTrailRenderer.enabled = false;
+        agent = GetComponent<NavMeshAgent>();
     }
     void Update()
     {
         //RaycastHit hit;
-        Move();
-        Jump();
+        if (CanMoveState())
+        {
+            Move();
+            Jump();
+        }
 
-        Dash();
+        bool isSuccessDash = Dash();
+        Attack(isSuccessDash);
+    }
+
+    private bool CanMoveState()
+    {
+        if (State == StateType.Attack)
+            return false;
+        if (State == StateType.TakeHit)
+            return false;
+        if (State == StateType.Death)
+            return false;
+        return true;
+    }
+
+    private void Attack(bool isSuccessDash)
+    {
+        if (isSuccessDash)
+        {
+            return;
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            StartCoroutine(AttackCo());
+        }
+    }
+
+    public float attackTime = 1;
+    public float attackApplyTime = 0.2f;
+    public LayerMask enemyLayer;
+    public SphereCollider attackCollider;
+    public float power = 10;
+    private IEnumerator AttackCo()
+    {
+        State = StateType.Attack;
+        yield return new WaitForSeconds(attackApplyTime);
+        // 실제 어택하는 부분.
+        var enemyColliders = Physics.OverlapSphere(attackCollider.transform.position,
+            attackCollider.radius, enemyLayer);
+        foreach (var item in enemyColliders)
+        {
+            item.GetComponent<Goblin>().TakeHit(power);
+        }
+
+        yield return new WaitForSeconds(attackTime);
+        State = StateType.Idle;
     }
     #region
 
@@ -40,33 +95,69 @@ public class Player : MonoBehaviour
     [Foldout("대시")] public float dashableTime = 0.4f;
     float mouseDownTime;
     Vector3 mouseDownPosition;
-    private void Dash()
+    private bool Dash()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             mouseDownTime = Time.time;
             mouseDownPosition = Input.mousePosition;
         }
-        if (nextDashableTime < Time.time)
+        if (nextDashableTime < Time.time) // 대쉬 가능한 시간이 현재 시간보다 작다면 실행 ( 대쉬 쿨타임 적용을 위함 )
         {
             if (Input.GetKeyUp(KeyCode.Mouse0))
             {
                 bool isDashDrag = IsSuccessDashDrag();
                 if (isDashDrag)
                 {
-                    nextDashableTime = Time.time + dashCoolTime;
+                    nextDashableTime = Time.time + dashCoolTime; // 대쉬가 실행된 현재 시간에 쿨타임 시간을 더해주기
                     StartCoroutine(DashCo());
+                    return true;
                 }
             }
         }
+        return false;
     }
+    public float hp = 100;
+
+    internal void TakeHit(int damage)
+    {
+        if (State == StateType.Death)
+        {
+            return;
+        }
+        hp -= damage;
+        // 피격 모션하자.
+
+        StartCoroutine(TakeHitCo());
+
+        // hp < 0 으면 죽자.
+    }
+
+    public float takeHitTime = 0.3f;
+    private IEnumerator TakeHitCo()
+    {
+        State = StateType.TakeHit;
+        yield return new WaitForSeconds(takeHitTime);
+        if (hp > 0)
+            State = StateType.Idle;
+        else
+            StartCoroutine(DeathCo());
+    }
+    public float deathTime = 0.5f;
+    private IEnumerator DeathCo()
+    {
+        State = StateType.Death;
+        yield return new WaitForSeconds(deathTime);
+        Debug.Log("게임 종료");
+    }
+
     [Foldout("대시")] public float dashTime = 0.3f;
     [Foldout("대시")] public float dashSpeedMultiplySpeed = 4f;
     Vector3 dashDirection;
     private IEnumerator DashCo()
     {
         // dashDirection x방향만 사용.
-        spriteTrailRenderer.enabled = true;
+        spriteTrailRenderer.enabled = true; // 대쉬하는 동안 트레일을 활성
         dashDirection = Input.mousePosition - mouseDownPosition;
         dashDirection.y = 0;
         dashDirection.z = 0;
@@ -84,19 +175,19 @@ public class Player : MonoBehaviour
     private bool IsSuccessDashDrag()
     {
         // 시간 체크
-        float dragTime = Time.time - mouseDownTime;
-        if (dragTime > dashableTime)
+        float dragTime = Time.time - mouseDownTime; // 마우스를 클릭을 땐 현재 시간과 처음 마우스를 눌렀던 시간의 차이 (총 drag 시간)
+        if (dragTime > dashableTime) // 총 drag시간이 설정한 dashableTime(0.4f)보다 작다면
         {
-            return false;
+            return false; // 대쉬를 실행 안함
         }
 
-        float dragDistance = Vector3.Distance(mouseDownPosition, Input.mousePosition);
-        if (dragDistance < dashableDistance)
+        float dragDistance = Vector3.Distance(mouseDownPosition, Input.mousePosition); // 총 drag한 길이
+        if (dragDistance < dashableDistance) // 총 drag한 길이가 대쉬가 가능한 거리 보다 작으면 
         {
-            return false;
+            return false;   // 대쉬를 실행안함
         }
 
-        return true;
+        return true;    // 그 이외의 상황들은 대쉬를 실행
     }
     #endregion
     [BoxGroup("점프")]public AnimationCurve jumpYac;
@@ -123,6 +214,8 @@ public class Player : MonoBehaviour
         JumpUp,
         JumpDown,
         Attack,
+        TakeHit,
+        Death,
         Dash,
     }
     StateType State
@@ -139,32 +232,39 @@ public class Player : MonoBehaviour
     }
     Animator animator;
     JumpStateType jumpState;
-    public float jumpYMultiply = 1;
-    public float jumpTimeMultiply = 1;
+    [BoxGroup("점프")] public float jumpYMultiply = 1;
+    [BoxGroup("점프")] public float jumpTimeMultiply = 1;
     private IEnumerator JumpCo()
     {
         jumpState = JumpStateType.Jump;
         State = StateType.JumpUp;
+        agent.enabled = false;
         float jumpStartTime = Time.time;
         float jumpDuration = jumpYac[jumpYac.length - 1].time;
         jumpDuration *= jumpTimeMultiply;
         float jumpEndTime = jumpStartTime + jumpDuration;
         float sumEvaluateTime = 0;
-        float previousY = 0;
+        float previousY = float.MinValue;
         while (Time.time < jumpEndTime)
         {
             float y = jumpYac.Evaluate(sumEvaluateTime / jumpTimeMultiply);
-            y *= jumpYMultiply;
+            y *= jumpYMultiply * Time.deltaTime;
             transform.Translate(0, y, 0);
             yield return null;
 
-            if (previousY > y)
+            if (previousY > transform.position.y)
             {
                 State = StateType.JumpDown;
             }
-            previousY = y;
+
+            if (transform.position.y < 0)
+            {
+                break;
+            }
+            previousY = transform.position.y;
             sumEvaluateTime += Time.deltaTime;
         }
+        agent.enabled = true;
         jumpState = JumpStateType.Ground;
         State = StateType.Idle;
     }
@@ -185,31 +285,21 @@ public class Player : MonoBehaviour
             {
                 moveableDistance = walkDistance;
             }
-
-            if (distance > moveableDistance)
+            Vector3 dir = hitPoint - transform.position;
+            if (state == StateType.Dash)
             {
-                var dir = hitPoint - transform.position;
-                dir.Normalize();
-                if (state == StateType.Dash)
-                {
-                    dir = dashDirection;
-                }
+                dir = dashDirection;
+            }
+            dir.Normalize();
+            if (distance > moveableDistance || State == StateType.Dash)
+            {
+                
                 transform.Translate(dir * speed * Time.deltaTime, Space.World);
 
                 // 방향(dir)에 따라서
                 // 오른쪽이라면 Y : 0, spriteX : 45
                 // 왼쪽이라면 Y : 180, spriteX : -45
-                bool isRightSide = dir.x > 0;
-                if (isRightSide)
-                {
-                    transform.rotation = Quaternion.Euler(Vector3.zero);
-                    spriteTr.rotation = Quaternion.Euler(45, 0, 0);
-                }
-                else
-                {
-                    transform.rotation = Quaternion.Euler(0, 180, 0);
-                    spriteTr.rotation = Quaternion.Euler(-45, -180, 0);
-                }
+                
                 if (ChangeableState())
                     State = StateType.Walk;
             }
@@ -217,6 +307,16 @@ public class Player : MonoBehaviour
             {
                 if (ChangeableState())
                     State = StateType.Idle;
+            }
+
+            bool isRightSide = dir.x > 0;
+            if (isRightSide)
+            {
+                transform.rotation = Quaternion.Euler(Vector3.zero);
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
             }
             bool ChangeableState()
             {
