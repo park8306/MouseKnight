fa4211976ce0973390e7d0b65e72fbb79f1afc98
park8306 <kosaf0804@gmail.com>
2021-07-15 21:26:00 +0900

    버튼, 텍스트 생성

---
+++ Assets/StageResultUI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageResultUI : BaseUI<StageResultUI>
+{
+    // Start is called before the first frame update
+    Text gradeText;
+    Text enemiesKilledText;
+    Text damageTakenText;
+    Button continueButton;
+    
+    void Start()
+    {
+        gradeText = transform.Find("GradeText").GetComponent<Text>();
+        enemiesKilledText = transform.Find("EnemiesKilledText").GetComponent<Text>();
+        damageTakenText = transform.Find("DamageTakenText").GetComponent<Text>();
+        continueButton = transform.Find("ContinueButton").GetComponent<Button>();
+        continueButton.AddListener(this, LoadNextStage);
+    }
+
+    private void LoadNextStage()
+    {
+        Debug.LogWarning("LoadNextStage");
+    }
+}
